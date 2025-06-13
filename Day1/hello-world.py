fec70d9db98110315ebe45ea7e76f989d487135d
Pranav <pranav.kumbhar02@gmail.com>
2025-06-13 21:34:33 +0530

    Day1 Complete

--- Day1/hello-world.py
+++ Day1/hello-world.py
@@ -1,1 +1,7 @@
 print('hello-World')
+
+name = input('Hello, what is your name? ')
+length = len(name)
+print(name)
+
+print(length)
---
+++ Projects/NameGenerator/bandNameGenerator.py
@@ -0,0 +1,9 @@
+print("Welcome to the Band Name Generator!")
+
+city = input("What is your City name?")
+print(city)
+pet = input("What is your Pet's Name?")
+print(pet)
+
+bandName = (city + " " + pet)
+print(bandName)
