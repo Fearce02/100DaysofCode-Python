7ca9e85e2d44b09b79e7f64ce67cb688afbac574
Pranav <pranav.kumbhar02@gmail.com>
2025-06-16 20:10:30 +0530

    if else mini project

---
+++ Day3/ifElse.py
@@ -0,0 +1,9 @@
+print("Welcome to the rollercoaster ride!")
+
+height = int(input("Please enter your height in cms"))
+
+if height > 130:
+    print("You can ride the rollercoaster!")
+    #Everything after the indentation ":" is a block of code.
+else:
+    print("Sorry you cannot ride the rollercoaster")    
