8ce870e273591f9f53dd26cbc53e18da00babc76
Pranav <pranav.kumbhar02@gmail.com>
2025-06-17 15:27:52 +0530

    Day4 Start

---
+++ Day4/Randomization.py
@@ -0,0 +1,16 @@
+import random
+
+# print(random.randint(10, 40))
+
+# random_number_0_to_1 = random.random() * 10
+# print(random_number_0_to_1)
+
+# random_float = random.uniform(1, 15)
+# print(random_float)
+
+random_number = random.randint(0, 1)
+
+if random_number == 0:
+    print("heads")
+else :
+    print("tails")    
