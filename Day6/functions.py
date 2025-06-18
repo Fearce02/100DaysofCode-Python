2500689eecd6b635c63ad75e58f2e186f44cf966
Pranav <pranav.kumbhar02@gmail.com>
2025-06-18 21:56:32 +0530

    Day6 Complete

--- Day6/functions.py
+++ Day6/functions.py
@@ -15,6 +15,7 @@ myfunction1() #This is how you call a function
 my_function(name)
 
 #indentation
+
 for prints in range(2): #this is the start of a block
     print("hello world") #This is inside the block.
 
---
+++ Day6/whileLoops.py
@@ -0,0 +1,12 @@
+#while loops run the code when the codition is currently true. When it changes the loop stops
+
+#while something is true
+#do this
+#then Do this
+#then do this
+
+number_of_prints = 0
+
+while number_of_prints < 20:
+    print("hello World")
+    number_of_prints += 1
