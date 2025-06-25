bd3ef0dd866053438181d86e4149577b526bf8b6
Pranav <pranav.kumbhar02@gmail.com>
2025-06-25 23:41:00 +0530

    Day11 complete

---
+++ Day11/blackjack.py
@@ -0,0 +1,7 @@
+#GameRules
+
+#the Goal of the game is to add up to the highest number without going over 21
+#if its added to a number more than 21 then its called a BUST.which means you lose immediately
+#all the cards that range from 2-10 are counted as their face value. Which means 2 = 2, 6= 6, 9=9
+#the Jack, king and Queen each count as 10
+#the ace can go from 1 to 11 depending on the value of your 21. you can decide its value
---
+++ Projects/Day11/Blackjack.py
@@ -0,0 +1,78 @@
+import random
+
+logo = """.------..------..------..------..------.     .------..------..------..------.
+|B.--. ||L.--. ||A.--. ||C.--. ||K.--. |.-.  |J.--. ||A.--. ||C.--. ||K.--. |
+| :(): || :/\: || (\/) || :/\: || :/\: ((5)) | :(): || (\/) || :/\: || :/\: |
+| ()() || (__) || :\/: || :\/: || :\/: |'-.-.| ()() || :\/: || :\/: || :\/: |
+| '--'B|| '--'L|| '--'A|| '--'C|| '--'K| ((1)) '--'J|| '--'A|| '--'C|| '--'K|
+`------'`------'`------'`------'`------'  '-'`------'`------'`------'`------'"""
+
+def deal_card():
+    cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
+    card = random.choice(cards)
+    return card
+
+def calculatescore(cards):
+    if sum(cards) and len(cards) == 2:
+        return 0
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+        
+    sum_of_cards = sum(cards)
+    return sum_of_cards
+
+def compare(u_score, comp_score):
+    if u_score == comp_score:
+        return "Draw!"
+    elif comp_score == 0:
+        return "You lose, The opponent has the Black Jack"
+    elif u_score == 0:
+        return "Win! With a Blackjack!"
+    elif u_score > 21:
+        return"You went Over! You lose."
+    elif comp_score > 21:
+        return "opponent went over, You Win!"
+    elif u_score > comp_score:
+        return " You Win!"
+    else:
+        return " You lose "
+    
+def play_game():
+    print(logo)
+    user_cards = []
+    computer_cards = []
+    is_game_over = False
+    computer_score = -1
+
+    for _ in range(2):
+        new_card = deal_card()
+        user_cards.append(new_card)
+        computer_cards.append(new_card)
+
+    while not is_game_over:
+        user_score = calculatescore(user_cards)
+        computer_score = calculatescore(computer_cards)
+        print(f"Your cards: {user_cards} Current Score: {user_score}")
+        print(f"Computer's First Card: {computer_cards[0]}")
+
+        if user_score == 0 or computer_score == 0 or user_score > 21:
+            is_game_over = True
+        else :
+            user_deal = input("Type 'y' to get another card or Type 'n' to pass: ").lower()
+            if user_deal == "y":
+                user_cards.append(deal_card())
+            else:
+                is_game_over = True
+
+    while computer_score != 0 and computer_score < 17:
+        computer_cards.append(deal_card())
+        computer_score = calculatescore(computer_cards)
+        
+    print(f"Your final hand: {user_cards} and Final Score: {user_score}")
+    print(f"Computer final hand: {computer_cards} and Computer score: {computer_score}")
+    print(compare(user_score, computer_score))
+
+while input("Do you want to play a game of BlackJack? Type 'y' or 'n': ") == "y":
+    print("\n " * 20)
+    play_game()
