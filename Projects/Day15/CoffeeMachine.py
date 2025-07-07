7cc988c484ec6d764fd5c230b0b4ea3ff35086e1
Pranav <pranav.kumbhar02@gmail.com>
2025-07-07 23:58:07 +0530

    Day15 Project CoffeeMachine

---
+++ Projects/Day15/CoffeeMachine.py
@@ -0,0 +1,53 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+# TODO: 1. Print a report of all CoffeeMachine Resources.
+
+profit = 0
+is_on = True
+
+while is_on:
+    user_choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
+    if user_choice == "off":
+        is_on = False
+    elif user_choice == "report":
+        print(f"Water: {resources['water']}")
+        print(f"Milk: {resources['milk']}")
+        print(f"Coffee: {resources['coffee']}")
+        print(f"Money: ${profit}")
+    else:
+        drink = MENU[user_choice]
+        print(drink)
+
+
+
+
